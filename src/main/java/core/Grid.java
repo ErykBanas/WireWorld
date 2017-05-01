386b37a2ddd56d2013fbb23808b4770edb059536
Mateusz <coctailparty@gmail.com>
2017-05-01 23:30:20 +0200

    Added basic implementation

--- src/main/java/core/Grid.java
+++ src/main/java/core/Grid.java
@@ -1,5 +1,20 @@
 package core;
 
 
+import java.util.HashMap;
+
 public class Grid {
+
+    private HashMap <Coordinate,Cell> grid;
+
+    public Grid(){
+        this.grid = new HashMap<Coordinate, Cell>();
+    }
+    public Grid(HashMap<Coordinate, Cell> grid) {
+        this.grid = grid;
+
+    }
+/* todo Dopisać gettery i settery oraz metody zmieniające stan komórki, pobierające stan,
+  zmieniające i pobierające liczbę sąsiadów*/
+
 }
