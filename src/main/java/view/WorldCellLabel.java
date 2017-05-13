2f390b76f375cdf15ff0d12ed422259ebcf6143b
Mateusz <coctailparty@gmail.com>
2017-05-13 21:33:22 +0200

    Added basic implementation

---
+++ src/main/java/view/WorldCellLabel.java
@@ -0,0 +1,18 @@
+package view;
+
+import core.Cell;
+
+/**
+ * Obiekty tej klasy reprezentują komórki na planszy interfejsu graficznego
+ */
+
+public class WorldCellLabel {
+
+    double x;
+    double y;
+    Cell.State state;
+
+    //todo Dodać metody odpowiedzalne za colorowanie komórek w zależności od stanu;
+
+
+}
