b660cc7f559c95a0f45c247cc1f4810eeaa7db0d
Mateusz <coctailparty@gmail.com>
2017-05-13 21:50:46 +0200

    Test class for application

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,11 @@
+import view.WireWorldView;
+import view.WireWorldViewGUI;
+
+public class AppTest {
+
+    public static void main(String []args){
+
+        WireWorldView view = new WireWorldViewGUI();
+        view.open();
+    }
+}
