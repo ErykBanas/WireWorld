6664bb1168a0d2a76608aa23bf8b6b1d2c1ab61b
Mateusz <coctailparty@gmail.com>
2017-05-15 16:04:18 +0200

    Added WorldCellLabel objects

--- src/main/java/view/WorldGridPanel.java
+++ src/main/java/view/WorldGridPanel.java
@@ -2,17 +2,34 @@ package view;
 
 
 import javax.swing.*;
+import java.awt.*;
 
 public class WorldGridPanel extends JPanel {
 
     private WorldCellLabel cellLabels [][];
 
-    public WorldGridPanel(int rows, int columns){
+    public WorldGridPanel(int rows, int columns, int preferredLabelSize){
 
         cellLabels = new WorldCellLabel[rows][columns];
+        setLayout(new GridLayout(rows,columns));
 
-        //todo Ustawić obiekty klasy WorldCellLabel na panelu
+        Dimension cellLabelSize = new Dimension(preferredLabelSize, preferredLabelSize);
 
+        for(int i = 0; i<rows; i++ ){
+            for(int j=0; j<columns; j++){
+                cellLabels[i][j] = new WorldCellLabel(i,j);
+                cellLabels[i][j].setOpaque(true);
+                cellLabels[i][j].setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY,1));
+                cellLabels[i][j].setPreferredSize(cellLabelSize);
+                add(cellLabels[i][j]);
+            }
+        }
+
+    }
+
+
+    public WorldCellLabel getCellLabel(int x, int y){
+        return this.cellLabels[x][y];
     }
 
 
