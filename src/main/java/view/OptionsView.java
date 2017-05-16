a98f8c0356436c03f170fb1c2786649e59e11f6a
Mateusz <coctailparty@gmail.com>
2017-05-16 11:53:22 +0200

    Interface for OptionsView

---
+++ src/main/java/view/OptionsView.java
@@ -0,0 +1,20 @@
+package view;
+
+import presenter.Presenter;
+
+public interface OptionsView {
+
+    void open();
+    void close();
+    void setPresenter(Presenter presenter);
+    Presenter getPresenter();
+
+    void setNumberOfGenerations(int numberOfGenerations);
+    int getNumberOfGenerations();
+    void setNumberOfCellsHorizontally(int numberOfCellsHorizontally);
+    int getNumberOfCellsHorizontally();
+    void setNumberOfCellsVertically(int numberOfCellsVertically);
+    int getNumberOfCellsVertically();
+
+
+}
