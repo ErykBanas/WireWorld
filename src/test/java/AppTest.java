c0ad11f4a1285520b1f4aba219d3aacb92b15325
Mateusz <coctailparty@gmail.com>
2017-05-16 18:09:00 +0200

    Presenter usage added

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,11 +1,17 @@
-import view.WireWorldView;
-import view.WireWorldViewGUI;
+import core.Cell;
+import presenter.Presenter;
+import presenter.WorldPresenter;
+import view.*;
 
 public class AppTest {
 
     public static void main(String []args){
 
+        Presenter presenter = new WorldPresenter();
         WireWorldView view = new WireWorldViewGUI();
+        view.setPresenter(presenter);
+        view.updateCellLabelColor(3,3, Cell.State.WIRE); //działa
         view.open();
+
     }
 }
