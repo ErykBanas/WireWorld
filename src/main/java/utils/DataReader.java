27733d3508fded37a1f90ef54979b7e9e34afe1b
Mateusz <coctailparty@gmail.com>
2017-05-17 10:26:58 +0200

    Comments added

--- src/main/java/utils/DataReader.java
+++ src/main/java/utils/DataReader.java
@@ -1,5 +1,22 @@
 package utils;
 
+import core.Cell;
+import core.Coordinate;
+
+import java.io.File;
+import java.util.HashMap;
+
+/**
+ * Interfejs reprezentuje obiekt wczytujący dane generacji do aplikacji
+ */
 
 public interface DataReader {
+
+    /**
+     * Wczytuje z pliku dane generacji do hashMapy
+     * @param file Plik, który został wskazany przez użytkownika do wczytania
+     * @return HashMapa z wczytanymi współrzędnymi komórek
+     */
+    HashMap<Coordinate,Cell> read(File file);
+
 }
