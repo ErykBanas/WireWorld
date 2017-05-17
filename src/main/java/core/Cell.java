4cd11f678ca360b426c9db50c33859ab2983b399
Mateusz <coctailparty@gmail.com>
2017-05-17 10:31:59 +0200

    Comments added

--- src/main/java/core/Cell.java
+++ src/main/java/core/Cell.java
@@ -1,6 +1,13 @@
 package core;
 
+/**
+ * Klasa reprezentuje pojedynczą komórkę w modelu z jej stanem
+ */
 public class Cell {
+
+    /**
+     * Typ wyliczeniowy: dostępne stany komórki
+     */
     public enum State {
         EMPTY,
         WIRE,
@@ -8,30 +15,58 @@ public class Cell {
         ELECTRONTAIL
     }
 
+    /**
+     * Aktualny stan komórki
+     */
     private State state;
+    /**
+     * Liczba sąsiadów komórki
+     */
     private int neighboursNumber;
 
 
+    /**
+     * Bezargumentowy konstruktor. Ustawia stan komórki na EMPTY i liczbę sąsiadów na 0.
+     */
     public Cell() {
         this.state = State.EMPTY;
         setNeighboursNumber(0);
     }
-
+    /**
+     * Konstruktor ustawiający stan komórki
+     * @param state Stan komórki typu wyliczeniowego {@link State}
+     */
     public Cell(State state) {
         this.state = state;
         setNeighboursNumber(0);
     }
 
+
+    /**
+     * Pobranie stanu komórki
+     * @return Stan komórki typu {@link State}
+     */
     public State getState() {
         return state;
     }
+    /**
+     * Ustawienie stanu komórki
+     * @param state Stan komórki typu {@link State}
+     */
     public void setState(State state) {
         this.state = state;
     }
-
+    /**
+     * Pobiera liczbę sąsiadów komórki
+     * @return Liczba sąsiadów komórki
+     */
     public int getNeighboursNumber() {
         return neighboursNumber;
     }
+    /**
+     * Ustawia liczbę sąsiadów komórki
+     * @param neighboursNumber Liczba sąsiadów komórki
+     */
     public void setNeighboursNumber(int neighboursNumber) {
         this.neighboursNumber = neighboursNumber ;
     }
