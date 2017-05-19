dc2ba3cfdb670659df04dac68f8f03f01d786754
Mateusz <coctailparty@gmail.com>
2017-05-19 15:44:50 +0200

    Return value changed

--- src/main/java/utils/DataReaderJSON.java
+++ src/main/java/utils/DataReaderJSON.java
@@ -2,6 +2,7 @@ package utils;
 
 import core.Cell;
 import core.Coordinate;
+import core.Grid;
 
 import java.io.File;
 import java.util.HashMap;
@@ -10,7 +11,7 @@ import java.util.HashMap;
  * Wczytuje dane generacji z pliku typu JSON do Hashmapy
  */
 public class DataReaderJSON implements DataReader {
-    public HashMap<Coordinate, Cell> read(File file) {
+    public Grid read(File file) {
         throw new UnsupportedOperationException("Jeszcze nie zaimplementowane");
     }
 }
