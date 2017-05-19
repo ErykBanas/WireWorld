1239d5986cd5373ad3bb9575c369581208ad8a40
Mateusz <coctailparty@gmail.com>
2017-05-19 15:45:21 +0200

    Return value changed

--- src/main/java/utils/DataReaderTxt.java
+++ src/main/java/utils/DataReaderTxt.java
@@ -2,13 +2,14 @@ package utils;
 
 import core.Cell;
 import core.Coordinate;
+import core.Grid;
 
 import java.io.File;
 import java.util.HashMap;
 
 public class DataReaderTxt implements DataReader{
 
-    public HashMap<Coordinate,Cell> read(File file){
+    public Grid read(File file){
         throw new UnsupportedOperationException("Jeszcze nie zaimplementowane");
     }
 }
