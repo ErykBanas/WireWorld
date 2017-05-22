764e9ecfb0fc5c5384f39b8f8a8875c9412ea703
Mateusz <coctailparty@gmail.com>
2017-05-22 15:56:05 +0200

    Descriptions added

--- src/main/java/utils/ConfigReader.java
+++ src/main/java/utils/ConfigReader.java
@@ -2,13 +2,39 @@ package utils;
 
 import java.io.File;
 
-
+/**
+ * Interfejs do obiektu pobierającego konfigurację aplikacji z pliku
+ */
 public interface ConfigReader {
 
+    /**
+     * Zwraca prędkość animacji
+     * @return Prędkość animacji
+     */
     Long getAnimationSpeed();
+
+    /**
+     * Zwraca liczbę generacji
+     * @return Liczba generacji
+     */
     Long getNumberOfGenerations();
+
+    /**
+     * Zwraca rozmiar poziomy planszy (gridu)
+     * @return Rozmiar poziomy planszy (gridu)
+     */
     Long getGridSizeX();
+
+    /**
+     * Zwraca rozmiar pionowy planszy (gridu)
+     * @return Rozmiar pionowy planszy (gridu)
+     */
     Long getGridSizeY();
+
+    /**
+     * Wczytuje dane konfiguracyjne z pliku i zapisuje w atrybutach klasy
+     * @param file Plik wejściowy z danymi konfiguracyjnymi
+     */
     void read(File file);
 
 
