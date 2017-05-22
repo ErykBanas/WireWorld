29a4b68d2c8d66e3ea74314bdbe5af5b5c7caac5
Mateusz <coctailparty@gmail.com>
2017-05-22 16:10:32 +0200

    Presenter usage added

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,17 +1,33 @@
-import core.Cell;
+import core.Grid;
+import model.WireWorld;
+import model.World;
 import presenter.Presenter;
 import presenter.WorldPresenter;
-import view.*;
+import utils.DataReader;
+import utils.DataReaderJSONDraft;
+import view.WireWorldView;
+import view.WireWorldViewGUI;
+
+import java.io.File;
 
 public class AppTest {
 
     public static void main(String []args){
 
+        Grid grid;
+        DataReader dr = DataReaderJSONDraft.getInstance();
+        File file = new File("input\\exampleInputData.json");
+        grid = dr.read(file);
+
+        World world = new WireWorld(grid);
         Presenter presenter = new WorldPresenter();
         WireWorldView view = new WireWorldViewGUI();
+
+        presenter.setWireWorldView(view);
+        presenter.setWorld(world);
         view.setPresenter(presenter);
-        view.updateCellLabelColor(3,3, Cell.State.WIRE); //działa
         view.open();
+
 
     }
 }
